0a979b387e947096a35fa2155826290d609faf42
irishWeb <luck.stas9@gmail.com>
2025-03-27 12:25:40 +0500

    init commit

---
+++ src/main/java/kz/iitu/csse241/lim/JobSearchApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.csse241.lim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/iitu/csse241/lim/JobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.csse241.lim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
